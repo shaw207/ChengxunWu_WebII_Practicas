eefc4ea1b10f1fc988af983c241c9c7d3c975f2b
shaw207 <wuandy272@gmail.com>
2026-04-13 20:58:11 +0200

    1.0.3
    - model

--- ejercicio/entregaIntermedia/src/models/Company.js
+++ ejercicio/entregaIntermedia/src/models/Company.js
@@ -1,1 +1,112 @@
-export {};
+import mongoose from 'mongoose';
+
+const addressSchema = new mongoose.Schema(
+  {
+    street: {
+      type: String,
+      trim: true,
+      default: null
+    },
+    number: {
+      type: String,
+      trim: true,
+      default: null
+    },
+    postal: {
+      type: String,
+      trim: true,
+      default: null
+    },
+    city: {
+      type: String,
+      trim: true,
+      default: null
+    },
+    province: {
+      type: String,
+      trim: true,
+      default: null
+    }
+  },
+  {
+    _id: false
+  }
+);
+
+const companySchema = new mongoose.Schema(
+  {
+    owner: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+      required: [true, 'El owner es obligatorio'],
+      index: true
+    },
+    name: {
+      type: String,
+      required: [true, 'El nombre es obligatorio'],
+      trim: true
+    },
+    cif: {
+      type: String,
+      required: [true, 'El CIF es obligatorio'],
+      unique: true,
+      uppercase: true,
+      trim: true,
+      index: true
+    },
+    address: {
+      type: addressSchema,
+      default: () => ({})
+    },
+    logo: {
+      type: String,
+      default: null
+    },
+    isFreelance: {
+      type: Boolean,
+      default: false
+    },
+    deleted: {
+      type: Boolean,
+      default: false,
+      index: true
+    },
+    deletedAt: {
+      type: Date,
+      default: null
+    }
+  },
+  {
+    timestamps: true,
+    versionKey: false
+  }
+);
+
+const excludeDeleted = function () {
+  if (!this.getOptions().withDeleted) {
+    this.where({ deleted: { $ne: true } });
+  }
+};
+
+companySchema.pre('find', excludeDeleted);
+companySchema.pre('findOne', excludeDeleted);
+companySchema.pre('findOneAndUpdate', excludeDeleted);
+companySchema.pre('countDocuments', excludeDeleted);
+
+companySchema.methods.softDelete = async function () {
+  this.deleted = true;
+  this.deletedAt = new Date();
+  return this.save();
+};
+
+companySchema.statics.findWithDeleted = function (filter = {}) {
+  return this.find(filter).setOptions({ withDeleted: true });
+};
+
+companySchema.statics.findOneWithDeleted = function (filter = {}) {
+  return this.findOne(filter).setOptions({ withDeleted: true });
+};
+
+const Company = mongoose.model('Company', companySchema);
+
+export default Company;
--- ejercicio/entregaIntermedia/src/models/User.js
+++ ejercicio/entregaIntermedia/src/models/User.js
@@ -1,1 +1,167 @@
-export {};
+import mongoose from 'mongoose';
+
+const addressSchema = new mongoose.Schema(
+  {
+    street: {
+      type: String,
+      trim: true,
+      default: null
+    },
+    number: {
+      type: String,
+      trim: true,
+      default: null
+    },
+    postal: {
+      type: String,
+      trim: true,
+      default: null
+    },
+    city: {
+      type: String,
+      trim: true,
+      default: null
+    },
+    province: {
+      type: String,
+      trim: true,
+      default: null
+    }
+  },
+  {
+    _id: false
+  }
+);
+
+const userSchema = new mongoose.Schema(
+  {
+    email: {
+      type: String,
+      required: [true, 'El email es obligatorio'],
+      unique: true,
+      lowercase: true,
+      trim: true,
+      index: true
+    },
+    password: {
+      type: String,
+      required: [true, 'La contrasena es obligatoria'],
+      select: false
+    },
+    name: {
+      type: String,
+      trim: true,
+      default: null
+    },
+    lastName: {
+      type: String,
+      trim: true,
+      default: null
+    },
+    nif: {
+      type: String,
+      trim: true,
+      uppercase: true,
+      default: null
+    },
+    role: {
+      type: String,
+      enum: ['admin', 'guest'],
+      default: 'admin',
+      index: true
+    },
+    status: {
+      type: String,
+      enum: ['pending', 'verified'],
+      default: 'pending',
+      index: true
+    },
+    verificationCode: {
+      type: String,
+      select: false,
+      default: null
+    },
+    verificationAttempts: {
+      type: Number,
+      default: 3
+    },
+    company: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Company',
+      index: true,
+      default: null
+    },
+    address: {
+      type: addressSchema,
+      default: () => ({})
+    },
+    refreshToken: {
+      type: String,
+      select: false,
+      default: null
+    },
+    refreshTokenExpiresAt: {
+      type: Date,
+      select: false,
+      default: null
+    },
+    deleted: {
+      type: Boolean,
+      default: false,
+      index: true
+    },
+    deletedAt: {
+      type: Date,
+      default: null
+    }
+  },
+  {
+    timestamps: true,
+    versionKey: false,
+    toJSON: {
+      virtuals: true,
+      transform: (doc, ret) => {
+        delete ret.password;
+        delete ret.verificationCode;
+        delete ret.refreshToken;
+        return ret;
+      }
+    },
+    toObject: {
+      virtuals: true
+    }
+  }
+);
+
+userSchema.virtual('fullName').get(function () {
+  return [this.name, this.lastName].filter(Boolean).join(' ');
+});
+
+const excludeDeleted = function () {
+  if (!this.getOptions().withDeleted) {
+    this.where({ deleted: { $ne: true } });
+  }
+};
+
+userSchema.pre('find', excludeDeleted);
+userSchema.pre('findOne', excludeDeleted);
+userSchema.pre('findOneAndUpdate', excludeDeleted);
+userSchema.pre('countDocuments', excludeDeleted);
+
+userSchema.methods.softDelete = async function () {
+  this.deleted = true;
+  this.deletedAt = new Date();
+  return this.save();
+};
+
+userSchema.statics.findWithDeleted = function (filter = {}) {
+  return this.find(filter).setOptions({ withDeleted: true });
+};
+
+userSchema.statics.findOneWithDeleted = function (filter = {}) {
+  return this.findOne(filter).setOptions({ withDeleted: true });
+};
+
+const User = mongoose.model('User', userSchema);
+
+export default User;
