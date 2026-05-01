3e3c2381c40a3d1977446305b41b6f0f5f0e0eab
shaw207 <wuandy272@gmail.com>
2026-05-01 21:51:40 +0200

    2.1.0

--- ejercicio/ejercicioFinal/src/index.js
+++ ejercicio/ejercicioFinal/src/index.js
@@ -1,7 +1,8 @@
 import { createServer } from 'node:http';
 import app from './app.js';
-import { connectDatabase, env } from './config/index.js';
-import { initSocket } from './config/socket.js';
+import { connectDatabase, disconnectDatabase, env } from './config/index.js';
+import { closeSocket, initSocket } from './config/socket.js';
+import { logger } from './services/logger.service.js';
 
 const startServer = async () => {
   await connectDatabase();
@@ -9,11 +10,30 @@ const startServer = async () => {
   initSocket(httpServer);
 
   httpServer.listen(env.PORT, () => {
-    console.log(`Servidor en http://localhost:${env.PORT}`);
+    logger.info({ port: env.PORT }, 'server started');
   });
+
+  const shutdown = async (signal) => {
+    logger.info({ signal }, 'graceful shutdown started');
+
+    httpServer.close(async () => {
+      try {
+        await closeSocket();
+        await disconnectDatabase();
+        logger.info('graceful shutdown finished');
+        process.exit(0);
+      } catch (error) {
+        logger.error({ err: error }, 'graceful shutdown failed');
+        process.exit(1);
+      }
+    });
+  };
+
+  process.on('SIGTERM', () => shutdown('SIGTERM'));
+  process.on('SIGINT', () => shutdown('SIGINT'));
 };
 
 startServer().catch((error) => {
-  console.error('Error al iniciar el servidor:', error.message);
+  logger.error({ err: error }, 'server startup failed');
   process.exit(1);
 });
